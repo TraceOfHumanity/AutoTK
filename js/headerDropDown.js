ba5c6b4da4e17c02d7705b9b5fd17ab1c8f05740
AzAlex <azalexxx02@gmail.com>
2022-10-07 02:22:15 -0700

    header

---
+++ js/headerDropDown.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('.header__menuItem').forEach(function (dropD) {
+  const btnDropDown = dropD.querySelector('.btnDropDown');
+  const headerDropdown = dropD.querySelector('.headerDropdown');
+  const arrow = dropD.querySelector('.arrow');
+
+  btnDropDown.addEventListener('click', function (e) {
+    this.nextElementSibling.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('hidden');
+    arrow.classList.toggle('arrow180')
+
+
+  })
+
+  document.addEventListener('click', function (event) {
+    if (event.target !== headerDropdown && event.target !== btnDropDown) {
+      headerDropdown.classList.remove('active')
+      headerDropdown.classList.add('hidden')
+      arrow.classList.remove('arrow180')
+    }
+  })
+
+})
