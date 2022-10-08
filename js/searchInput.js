551a1d5665525be1c0f8baea8cde71b34b70f53a
AzAlex <azalexxx02@gmail.com>
2022-10-07 20:15:57 -0700

    fix search

--- js/searchInput.js
+++ js/searchInput.js
@@ -1,13 +1,15 @@
 const headerLink = document.querySelector('.header__link');
 const searchInput = document.querySelector('.input');
 const searchIcon = document.querySelector('.button__search')
+const coverSearh = document.querySelector('.search__input')
+const inputIn = document.querySelector('.inputIn')
 
 headerLink.addEventListener('click', function(e) {
     searchInput.classList.toggle('hidden')
 })
 
 document.addEventListener('click', function (event) {
-  if (event.target !== headerLink && event.target !== searchInput || event.target == searchIcon) {
+  if (event.target !== inputIn && event.target !== headerLink) {
     searchInput.classList.add('hidden')
   }
 })
