1d81879aa57d2b94370f5add30df35e14ff0ef98
AzAlex <azalexxx02@gmail.com>
2022-10-10 20:52:21 -0700

    sliders3

--- js/sliderSettings.js
+++ js/sliderSettings.js
@@ -3,9 +3,24 @@ $(document).ready(function(){
     slidesToShow: 5,
     slidesToScroll: 1,
     infinite: true,
-
+    // initialSlide: 3,
     focusOnSelect: true,
     // centerMode: true,
   });
-
+  $('.reviews__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    // initialSlide: 3,
+    focusOnSelect: true,
+    // centerMode: true,
+  });
+  $('.news__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    // initialSlide: 3,
+    focusOnSelect: true,
+    // centerMode: true,
+  });
 });
