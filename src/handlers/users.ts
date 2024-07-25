83d3b16f0d513880af206426b7179beddedf0f10
Alexander_Ovchar <azalexxx02@gmail.com>
2024-07-25 18:49:04 +0300

    feat: Add user-related types, DTO, and handler functions

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,4 +1,7 @@
-import {Request, Response} from "express";
+import {Request, Response} from "express-serve-static-core";
+import {CreateUserDto} from "../dtos/CreateUser.dto";
+import {CreateUserQueryParams} from "../types/query-params";
+import {User} from "../types/response";
 
 export const getUsers = async (req: Request, res: Response) => {
   res.send([]);
@@ -8,6 +11,13 @@ export const getUserById = async (req: Request, res: Response) => {
   res.send({});
 };
 
-export const createUser = async (req: Request, res: Response) => {
-  res.send({});
+export const createUser = async (
+  req: Request<{}, {}, CreateUserDto, CreateUserQueryParams>,
+  res: Response<User>
+) => {
+  return res.status(201).send({
+    id: 1,
+    username: "test",
+    email: "dffddsgsfgfs@dfasdfdasf.com",
+  });
 };
